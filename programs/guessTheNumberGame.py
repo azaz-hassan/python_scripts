021437c6487a65585c466a8bba134ac51ed2576b
azaz-hassan <azazhassan75@gmail.com>
2021-05-10 15:42:21 +0000

    Made a guess the number simple game.

---
+++ programs/guessTheNumberGame.py
@@ -0,0 +1,21 @@
+import random
+real_guess = random.randint(1, 20)
+
+# Store player name
+name = input()
+print(f"Hi {name}, Welcome to the Guess Game")
+
+print("You have 7 guesses")
+num = 0
+for num in range(1, 7):
+    print("Take a guess: ")
+    guess = input()
+    if int(guess) > real_guess:
+        print("Your guess is high.")
+    elif int(guess) < real_guess:
+        print("Your guess is low.")
+    else:
+        print("Congrats, You guessed the number !!")
+        break
+
+print(f'{name} !! You guessed the number in {num} guesses')
