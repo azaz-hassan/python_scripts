4e43d22d1949dc3121bd10d694d20c25a322e085
azaz-hassan <azazhassan75@gmail.com>
2021-05-10 15:44:13 +0000

    Added the prompt: Hi, What is your name

--- programs/guessTheNumberGame.py
+++ programs/guessTheNumberGame.py
@@ -2,13 +2,14 @@ import random
 real_guess = random.randint(1, 20)
 
 # Store player name
+print("Hi, What is your name?")
 name = input()
 print(f"Hi {name}, Welcome to the Guess Game")
 
 print("You have 7 guesses")
 num = 0
 for num in range(1, 7):
-    print("Take a guess: ")
+    print("Take a guess ")
     guess = input()
     if int(guess) > real_guess:
         print("Your guess is high.")
